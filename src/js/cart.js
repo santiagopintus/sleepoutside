2f35e09b8f8c8fec8af1eb0cecb1c72babdca0bf
Santiago Pintus <santiagopintus@gmail.com>
2022-02-26 11:25:59 -0300

    Format and lint all files. (Replaced double quotes by single in most cases)

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,11 +3,10 @@ function getLocalStorage(key) {
 }
 
 function getCartContents() {
-  let markup = "";
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+  document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  document.querySelector('.product-list').innerHTML = renderCartItem(cartItems);
 }
 
 function renderCartItem(item) {
--- src/js/product.js
+++ src/js/product.js
@@ -5,7 +5,7 @@ function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw new Error('Bad Response');
   }
 }
 
@@ -18,7 +18,7 @@ function getLocalStorage(key) {
 
 // get tents data
 function getProductsData() {
-  fetch("../json/tents.json")
+  fetch('../json/tents.json')
     .then(convertToJson)
     .then((data) => {
       products = data;
@@ -32,17 +32,17 @@ function getProductsData() {
 // add to cart button event handler
 function addToCart(e) {
   //Get current cart to push new items to
-  currentCart = getLocalStorage("so-cart");
+  let currentCart = getLocalStorage('so-cart');
   if (currentCart) {
     cart = currentCart;
   }
   const product = products.find((item) => item.Id === e.target.dataset.id);
   //insert current cart plus new product
   cart.push(product);
-  setLocalStorage("so-cart", cart);
+  setLocalStorage('so-cart', cart);
   cart = [];
 }
 
 getProductsData();
 // add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCart);
+document.getElementById('addToCart').addEventListener('click', addToCart);
