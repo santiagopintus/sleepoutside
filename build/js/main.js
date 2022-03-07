3c26f3bacff28f22eb2178f220a0ebca1d604849
jdbench <justin.d.bench@gmail.com>
2022-03-07 15:12:39 -0700

    completed individual assignment 4

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+import t from"./productData.js";import o from"./productList.js";import{loadHeaderFooter as r}from"./utils.js";r();const e=new t("tents"),s=document.querySelector(".product-list"),c=new o("tents",e,s);c.init();
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,11 @@
+function convertToText(res) {
+  if (res.ok) {
+    return res.text();
+  } else {
+    throw new Error('Bad Response');
+  }
+}
+
 // wrapper for querySelector...returns matching element
 export function qs(selector) {
   return document.querySelector(selector);
@@ -24,4 +32,40 @@ export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
+}
+
+export function renderListWithTemplate(template, parent, list, callback) {
+  list.forEach(item => {
+    const clone = template.content.cloneNode(true);
+    const templateWithData = callback(clone, item);
+    parent.appendChild(templateWithData);
+  })
+}
+
+export function renderWithTemplate(template, parent, data, callback) {
+  
+    let clone = template.content.cloneNode(true);
+    if(callback) {
+    clone = callback(clone, data);
+    
+    }
+    parent.appendChild(clone);
+  
+}
+
+export async function loadTemplate(path) {
+  const html = await fetch(path).then(convertToText);
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template;
+
+}
+
+export async function loadHeaderFooter() {
+  const header = await loadTemplate('../partials/header.html');
+  const footer = await loadTemplate('../partials/footer.html');
+  const headerElement = document.getElementById('main-header');
+  const footerElement = document.getElementById('main-footer');
+  renderWithTemplate(header, headerElement);
+  renderWithTemplate(footer, footerElement);
 }
